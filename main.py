94776a3fff2e886170c8a66818a70cc520e542f4
Nanatsu2370 <94955661+Nanatsu2370@users.noreply.github.com>
2022-04-23 22:08:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -241,7 +241,7 @@ def onmessage(update,bot:ObigramClient):
         tl_admin_user = os.environ.get('tl_admin_user')
 
         #set in debug
-        tl_admin_user = 'FriendXfriendss'
+        tl_admin_user = 'RayCruzMDeportivo'
 
         jdb = JsonDatabase('database')
         jdb.check_create()
@@ -579,7 +579,7 @@ def main():
     bot_token = os.environ.get('bot_token')
 
     #set in debug
-    bot_token = '5319657829:AAH_iGsBZzeVa0vvBJwgEObiuQMRZCF_GKM'
+    bot_token = '5369698097:AAE5EjpB5bxrmvnTDAMBG5RugUYxw1_HB0I'
 
     bot = ObigramClient(bot_token)
     bot.onMessage(onmessage)
